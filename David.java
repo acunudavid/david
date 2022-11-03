3c7c2ce217dc124b409b5fd5f3c9cd4c64367b20
acunudavid <117373829+acunudavid@users.noreply.github.com>
2022-11-03 18:33:25 +0300

    Add files via upload

---
+++ David.java
@@ -0,0 +1,18 @@
+package davidassignment;
+
+public class David { 
+		public static void main(String[] args)   
+		{  
+		int num = 100, i = 1, sum = 0;  
+		//executes until the condition returns true  
+		while(i <= num)  
+		{  
+		//adding the value of i into sum variable  
+		sum = sum + i;  
+		//increments the value of i by 1  
+		i++;  
+		}  
+		//prints the sum   
+		System.out.println("Sum of First 100 Natural Numbers is = " + sum);  
+		}  
+		}  
